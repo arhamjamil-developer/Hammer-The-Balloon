6dbbc2505e16d6b0c500801cf045510ced394b6a
arhamjamil-developer <arhamthoughts@gmail.com>
2025-10-01 23:23:09 +0500

    Add Movement Script with Debug Hello

---
+++ Assets/Movement.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    
+    public void Hello()
+    {
+        //GitHub Example Saying Hello To Umair
+        Debug.Log("Hello Umair");
+    }
+
+
+
+}
